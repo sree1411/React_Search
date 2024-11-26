f133f2b40f0e71ef31d68ee82df1618e15152d1a
sreekanth <sreekanth>
2024-11-26 17:03:14 +0530

    search function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [photos, setPhotos] = useState([]);
+  const [query, setQuery] = useState("");
+  const [filteredProduct, setFilteredProduct] = useState([]);
+
+  useEffect(() => {
+    const fetchPhotos = async () => {
+      let response = await fetch(
+        "https://jsonplaceholder.typicode.com/photos?_page=1&_limit=40"
+      );
+      let data = await response.json();
+      setPhotos(data);
+      setFilteredProduct(data);
+    };
+    fetchPhotos();
+  }, []);
+
+  useEffect(() => {
+    let res = photos.filter((pht) =>
+      pht.title.toLowerCase().includes(query.toLowerCase())
+    );
+    setFilteredProduct(res);
+  }, [photos,query]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1> Search Functionality:{query}</h1>
+      <input
+        type="text"
+        placeholder="Enter the Title"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        style={{ width:"100%", padding:"10px", fontSize:"30px", marginBottom:"10px"}}
+      />
+      <div style={{display:"grid", gridTemplateColumns:"repeat(3,1fr)", gap:"10px"}}>
+        {filteredProduct.map((photo) => (
+          <div key={photo.id} style={{textAlign:"center", padding:"10px", border:"1px solid black", borderRadius:"10px"}}>
+            <img src={photo.thumbnailUrl} alt={photo.thumbnailUrl} 
+                 style={{ width: "100px", height: "100px", marginBottom: "10px" }}
+            />
+             <h5 style={{fontFamily:"sans-serif"}}>Title:{photo.title}</h5>
+          </div>
+         
+        ))}
+      </div>
     </div>
   );
-}
+} 
 
-export default App;
+export default App; 
